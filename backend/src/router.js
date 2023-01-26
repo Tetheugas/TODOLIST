a74adce594c34cd1eae2a35c17582ac94bb35670
Matheus Silva <98681828+Tetheugas@users.noreply.github.com>
2023-01-26 11:13:56 -0300

    TodoList full stack

---
+++ backend/src/router.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const app = express()
+
+//router.get('/tasks', (req, res)=> res.status(200).send('O router esta funcionando!'))
+
+
+module.exports = router
+
